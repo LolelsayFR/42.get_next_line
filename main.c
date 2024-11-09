396cea1969066c9641790cc6f33ca248683d21d4
Emilien Maillet <emaillet@c1r7p10.42lehavre.fr>
2024-11-09 02:23:17 +0100

    debug avencement

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: emaillet <emaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 04:26:13 by emaillet          #+#    #+#             */
-/*   Updated: 2024/11/09 00:43:42 by emaillet         ###   ########.fr       */
+/*   Updated: 2024/11/09 02:02:47 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 #include <fcntl.h>
 
 void	ft_putstr(char *s)
@@ -35,16 +36,17 @@ int	main(void)
 	fd1 = open("labible", O_RDONLY);
 	fd2 = open("test2", O_RDONLY);
 	fd3 = open("test3", O_RDONLY);
-	while (i <= 1)
+	while (i <= 3)
 	{
 		if (i == 1)
 			str = get_next_line(fd1);
 		else if (i == 2)
 			str = get_next_line(fd2);
 		else
-			str = get_next_line(fd3);
-		if (str)
-			ft_putstr(str);
+			str = get_next_line(1);
+		printf("%s", str);
+		//if (str)
+			//ft_putstr(str);
 		free(str);
 		if (!str)
 			i++ ;
