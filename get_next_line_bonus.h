47c1535cffcc544e9319da20bce65023ac152224
Lolelsay <maillet.emilien@gmail.com>
2024-11-27 19:13:39 +0100

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -26,15 +26,15 @@ typedef struct s_strlist
 	struct s_strlist	*next;
 }	t_strlist;
 
-void	stash_free(t_list **stash);
-int		stash_strlinelen(t_list *stash);
-void	stash_load(t_list *stash, char **str);
-void	stash_save(int fd, t_list **stash);
+void	stash_free(t_strlist **stash);
+int		stash_strlinelen(t_strlist *stash);
+void	stash_load(t_strlist *stash, char **str);
+void	stash_save(int fd, t_strlist **stash);
 char	*get_next_line(int fd);
 int		ft_strichr(const char *s, int c);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *lnew);
-t_list	*ft_lstnew_str(char *str, int count);
+t_strlist	*ft_lstlast(t_strlist *lst);
+void	ft_lstadd_back(t_strlist **lst, t_strlist *lnew);
+t_strlist	*ft_lstnew_str(char *str, int count);
 char	*ft_substrlen(const char *s, unsigned int start);
 
 #endif
